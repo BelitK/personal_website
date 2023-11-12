ebf0dede6d6e165d4c17ff1f09d88d4218dc4ee9
belitk <belitberdelk@gmail.com>
2023-11-13 02:31:32 +0300

    added blockchain example and streamlit

--- fastapi/app.py
+++ fastapi/app.py
@@ -3,12 +3,14 @@ from typing import List
 from fastapi import FastAPI, Request
 from db.database import DataB, Certificate
 from functions.func import Fire, Human
+from functions.block import Blockchain
 
 
 Certificates, database = DataB().stuff()
 app = FastAPI()
 
 Fire, Human = Fire(), Human()
+blockchain = Blockchain()
 
 
 @app.on_event("startup")
@@ -47,3 +49,56 @@ async def human_detection(request: Request):
     a = await request.json()
     image = json.loads(a)["b64"]
     return Human.get_human(image)
+
+
+@app.get("/mine_block/{data}/{user}/{workT}/{run}")
+def mine_block(data, user, workT, run):
+    # get the data we need to create a block
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block["proof"]
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+
+    block = blockchain.create_blockchain(proof, previous_hash, data, user, workT, run)
+    response = {
+        "message": "Block mined!",
+        "index": block["index"],
+        "timestamp": block["timestamp"],
+        "Username": block["Username"],
+        "type": block["type"],
+        "data": block["data"],
+        "workTime": block["workTime"],
+        "proof": block["proof"],
+        "previous_hash": block["previous_hash"],
+    }
+    blockchain.mine_blocks()
+    return json.dumps(response)
+
+
+@app.get("/get_chain")
+def get_chain():
+    response = {"chain": blockchain.chain, "length": len(blockchain.chain)}
+    return json.dumps(response)
+
+
+@app.get("/find_user/{usrname}")
+def get_user(usrname):
+    response = []
+    for data in blockchain.chain:
+        if data["Username"] == usrname:
+            response.append(data)
+    return json.dumps(response)
+
+
+@app.get("/find_type/{type2}")
+def get_type(type2):
+    response = []
+    for data in blockchain.chain:
+        if data["type"] == type2:
+            response.append(data)
+    return json.dumps(response)
+
+
+@app.get("/validate")
+def validate():
+    return blockchain.is_chain_valid(blockchain.chain)
