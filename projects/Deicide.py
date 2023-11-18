50509a99e8e301629cb586d700890763bd11c498
belitk <belitberdelk@gmail.com>
2023-11-18 20:46:08 +0300

    stop streamlit gonna change libs

--- projects/Deicide.py
+++ projects/Deicide.py
@@ -5,71 +5,71 @@ import requests
 
 st.title("Chain test !&!")
 
-st.write("Insert data to chain")
-username = st.text_input("Username")
-data = st.text_input("Data to input")
-workTime = round(st.number_input("Worked time"), 2)
-choice = st.selectbox("run types", ["success", "failure", "canceled"])
-clicked = st.button("send to chain")
+# st.write("Insert data to chain")
+# username = st.text_input("Username")
+# data = st.text_input("Data to input")
+# workTime = round(st.number_input("Worked time"), 2)
+# choice = st.selectbox("run types", ["success", "failure", "canceled"])
+# clicked = st.button("send to chain")
 
-if clicked:
-    r = requests.get(
-        f"http://api:1000/mine_block/{data}/{username}/{workTime}/{choice}"
-    )
-    st.json(json.loads(r.content))
+# if clicked:
+#     r = requests.get(
+#         f"http://api:1000/mine_block/{data}/{username}/{workTime}/{choice}"
+#     )
+#     st.json(json.loads(r.content))
 
-"------------------------------"
-"gonna add pending blocks for multi-worker type things"
-# mine = st.button("click to mine")
-# if mine:
-#        r=requests.get(f"http://api:1000/mine_block")
-#        st.json(json.loads(r.text))
-"--------------------------------------------------------------"
+# "------------------------------"
+# "gonna add pending blocks for multi-worker type things"
+# # mine = st.button("click to mine")
+# # if mine:
+# #        r=requests.get(f"http://api:1000/mine_block")
+# #        st.json(json.loads(r.text))
+# "--------------------------------------------------------------"
 
-st.write("Get chain and show")
-get_chain = st.button("Get chain")
-chain = []
-if get_chain:
-    chain = requests.get(f"http://api:1000/get_chain")
-    st.json(json.loads(chain.content))
+# st.write("Get chain and show")
+# get_chain = st.button("Get chain")
+# chain = []
+# if get_chain:
+#     chain = requests.get(f"http://api:1000/get_chain")
+#     st.json(json.loads(chain.content))
 
-"--------------------------------------------------------------"
+# "--------------------------------------------------------------"
 
-st.write("Find data with user")
-usern = st.text_input("user to find")
-find = st.button("click to find")
+# st.write("Find data with user")
+# usern = st.text_input("user to find")
+# find = st.button("click to find")
 
-with st.spinner("Waitt"):
-    if find:
-        r = requests.get(f"http://api:1000/find_user/{usern}")
-        st.json(json.loads(r.content))
+# with st.spinner("Waitt"):
+#     if find:
+#         r = requests.get(f"http://api:1000/find_user/{usern}")
+#         st.json(json.loads(r.content))
 
-"---------------------------------------------------"
+# "---------------------------------------------------"
 
-st.write("find data with run type")
+# st.write("find data with run type")
 
-choice2 = st.selectbox("run types2", ["success", "failure", "canceled"])
-find2 = st.button("click to find with type")
+# choice2 = st.selectbox("run types2", ["success", "failure", "canceled"])
+# find2 = st.button("click to find with type")
 
-if find2:
-    r = requests.get(f"http://api:1000/find_type/{choice2}")
-    st.json(json.loads(r.content))
+# if find2:
+#     r = requests.get(f"http://api:1000/find_type/{choice2}")
+#     st.json(json.loads(r.content))
 
-"------------------------------"
+# "------------------------------"
 
-valid = st.button("click to validate chain")
-if valid:
-    r = requests.get(f"http://api:1000/validate")
-    st.write(r.text)
-"---------------------------------------"
-st.write("Download chain")
+# valid = st.button("click to validate chain")
+# if valid:
+#     r = requests.get(f"http://api:1000/validate")
+#     st.write(r.text)
+# "---------------------------------------"
+# st.write("Download chain")
 
-chain2 = requests.get(f"http://api:1000/get_chain")
+# chain2 = requests.get(f"http://api:1000/get_chain")
 
 
-st.download_button(
-    label="Download data as CSV",
-    data=json.loads(chain2.content),
-    file_name="chain.json",
-    mime="text/json",
-)
+# st.download_button(
+#     label="Download data as CSV",
+#     data=json.loads(chain2.content),
+#     file_name="chain.json",
+#     mime="text/json",
+# )
