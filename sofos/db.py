d49fd58eb8ec8326594c57404427a6fba18b5a02
belitk <belitberdelk@gmail.com>
2023-12-16 02:03:03 +0300

    finally

--- sofos/db.py
+++ sofos/db.py
@@ -12,48 +12,60 @@ class db:
             user="belit",
             password="12897",
         )
+        self.conn.autocommit = True
 
     def get_cursor(self):
+        self.__init__()
         self.curr = self.conn.cursor()
 
     def fetchh(self):
         return self.curr.fetchall()
 
     def get_token(self, number: str, just_token=False):
-        if number!='0' and number!=''and number.isdigit():
+        if number != "0" and number != "" and number.isdigit():
             self.get_cursor()
-            
+
             self.curr.execute(f"select * from guests where renum={number};")
             data = self.curr.fetchall()[0]
-            print(data)
-            if data[4]=='False':
-                self.curr.execute(f'update guests set checkin=True where renum={number}')
-                self.conn.commit()
-                
+            #print(data)
+            if data[4] == "False":
+                self.curr.execute(
+                    f"update guests set checkin=True where renum={number}"
+                )
+                # self.conn.commit()
+
             self.curr.close()
             if just_token:
-                return data[6]
+                return data[6], str(data[5])
 
             print(f"{str(data[5])} has {data[6]} tokens")
-            return data[6], data[1],str(data[5]), 'OK' if str(data[4])=='true' else 'None'
-        return 'No Name', 'No Number', 'No Code', 'No Checkin'
-    
-    def update_token(self,number:str, tokens:int):
+            self.conn.close()
+            return (
+                data[6],
+                data[1],
+                str(data[5]),
+                "OK" if str(data[4]) == "true" else "None",
+            )
+        self.conn.close()
+        return "No Name", "No Number", "No Code", "No Checkin"
+
+    def update_token(self, number: str, tokens: int):
         self.get_cursor()
         print("update tokennnnnn")
-        print(number)
-        self.curr.execute(f'update guests set tokens={tokens} where renum={number}')
+        #print(number)
+        self.curr.execute(f"update guests set tokens={tokens} where renum={number}")
         self.conn.commit()
-        print(f"{tokens} added to {number}")
+        #print(f"{tokens} added to {number}")
         self.curr.close()
+        self.conn.close()
 
     # generate_qr(person[5])
+
 
 class person:
     def __init__(self) -> None:
         self.renum = "0"
-        self.oldrenum="0"
-        self.token="0"
+        self.oldrenum = "0"
+        self.token = "0"
         self.name = "None"
-        self.checkin='False'
-
+        self.checkin = "False"
--- sofos/main.py
+++ sofos/main.py
@@ -1,17 +1,10 @@
-import os
-import time
 from db import db, person
-from flask import Flask, render_template, request, jsonify, Response, redirect, url_for
-import random
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+
 app = Flask(__name__)
 db_con = db()
 person = person()
 
-# person.name=str(random.randint(0,10))
-# person.renum=str(random.randint(0,10))
-# person.token=str(random.randint(0,10))
-# person.checkin='False'
-# print(f"{'Isim = '+person.name+' Numara = '+person.renum+' Jeton = '+str(person.token)+' Checkin ='+person.checkin}")
 
 @app.route("/")
 def home():
@@ -23,28 +16,40 @@ def kommunity():
     return render_template("Kommunity.html")
 
 
-@app.route('/qr')
+@app.route("/qr")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/process', methods=['POST'])
+
+@app.route("/process", methods=["POST"])
 def process_qr_code():
     data = request.get_json()
-    content = data.get('qr_data', '')
-    person.renum=content
-    result = {"status": "success", "message": f"{'Isim = '+person.name+', Numara = '+person.renum+', Jeton = '+str(person.token)+', Checkin ='+person.checkin}","renum":person.renum}
-    if person.oldrenum!=person.renum:
-        #person.renum=qr_code
+    content = data.get("qr_data", "")
+    person.renum = content
+    result = {
+        "status": "success",
+        "message": f"{'Isim = '+person.name+', Numara = '+person.renum+', Jeton = '+str(person.token)+', Checkin ='+person.checkin}",
+        "renum": person.renum,
+    }
+    if person.oldrenum != person.renum:
+        # person.renum=qr_code
         # print(person.token)
-        person.oldrenum=person.renum
-        person.token, person.name, person.renum, person.checkin=db_con.get_token(person.renum)
-        result = {"status": "success", "message": f"{'Isim = '+person.name+', Numara = '+person.renum+', Jeton = '+str(person.token)+', Checkin ='+person.checkin}","renum":f"{person.renum}"}
-    print('get tokennn')
-    print(person.renum)
+        person.oldrenum = person.renum
+        person.token, person.name, person.renum, person.checkin = db_con.get_token(
+            person.renum
+        )
+        result = {
+            "status": "success",
+            "message": f"{'Isim = '+person.name+', Numara = '+person.renum+', Jeton = '+str(person.token)+', Checkin ='+person.checkin}",
+            "renum": f"{person.renum}",
+        }
+    # print('get tokennn')
+    # print(person.token)
     # Process the QR code content here (e.g., print or save it)
-    #print(f"QR Code Content: {content}")
+    # print(f"QR Code Content: {content}")
 
     return jsonify(result)
+
 
 # @app.route('/stream')
 # def stream():
@@ -82,19 +87,21 @@ def process_qr_code():
 #     print(f"Received message: {eventStream()}")
 #     socketio.emit('display_text', eventStream(), broadcast=True)
 
-@app.route('/token', methods = ['POST', 'GET'])
+
+@app.route("/token", methods=["POST", "GET"])
 def token():
-    if request.method == 'POST':
+    if request.method == "POST":
         form_data = request.form
-        print('formdata::::')
-        print(form_data['renum'])
-        renum_num= form_data['renum']
-        if form_data['token'] == '':
+        # print('formdata::::')
+        renum_num = form_data.get("renum")
+        # print(form_data)
+        if form_data["token"] == "":
             return redirect(url_for("index"))
-        tokenn = db_con.get_token(number=renum_num,just_token=True)
-        person.token = str(int(tokenn)+int(form_data['token']))
-        db_con.update_token(person.renum, person.token)
+        tokenn, numm = db_con.get_token(number=renum_num, just_token=True)
+        tokenn = str(int(tokenn) + int(form_data["token"]))
+        db_con.update_token(numm, tokenn)
         return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port="8000")
